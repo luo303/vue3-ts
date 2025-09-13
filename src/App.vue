e22eb0764eab7171dc70e5989f927ee325dd5790
xhh <jiatangxi@qq.com>
2025-09-13 20:05:37 +0800

    svg矢量图标配置

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log(import.meta.env)
+</script>
 
 <template>
   <h1>You did it!</h1>
   <p>我是根组件</p>
+  <svg style="width: 30px; height: 30px">
+    <use xlink:href="#icon-photo" fill="red"></use>
+  </svg>
 </template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]'
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve('./src')
