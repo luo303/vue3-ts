e5c9e2cf1aac147a1b3272bcc82634dfabcbb0a0
xhh <jiatangxi@qq.com>
2025-09-13 21:01:30 +0800

    给svg图标使用添加全局组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
+import SvgIcon from './components/SvgIcon.vue'
 console.log(import.meta.env)
 </script>
 
 <template>
   <h1>You did it!</h1>
   <p>我是根组件</p>
-  <svg style="width: 30px; height: 30px">
-    <use xlink:href="#icon-photo" fill="red"></use>
-  </svg>
+  <SvgIcon name="home" color="red" width="100px" height="100px"></SvgIcon>
 </template>
 
 <style scoped></style>
---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,28 @@
+<template :style="{ width, height }">
+  <svg>
+    <use :xlink:href="prefix + name" :fill="color"></use>
+  </svg>
+</template>
+<script setup lang="ts">
+defineProps({
+  prefix: {
+    type: String,
+    default: '#icon-'
+  },
+  name: {
+    type: String
+  },
+  color: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: String,
+    default: '16px'
+  },
+  height: {
+    type: String,
+    default: '16px'
+  }
+})
+</script>
