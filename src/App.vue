27d52d22a7352cb37d24631ae2ee4b799d410ed0
xhh <jiatangxi@qq.com>
2025-09-14 10:35:32 +0800

    在src下添加index.ts文件将SvgIcon注册为全局组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import SvgIcon from './components/SvgIcon.vue'
 console.log(import.meta.env)
 </script>
 
 <template>
   <h1>You did it!</h1>
   <p>我是根组件</p>
-  <SvgIcon name="home" color="red" width="100px" height="100px"></SvgIcon>
+  <SvgIcon name="home" color="blue" width="100px" height="100px"></SvgIcon>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'virtual:svg-icons-register'
+import gloablComponent from '@/index'
 
 import App from '@/App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(gloablComponent)
 app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
